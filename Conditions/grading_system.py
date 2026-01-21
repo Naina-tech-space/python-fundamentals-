5176a361778c9062cb29adf25415953aa883eced
Naina-tech-space <bnaina153@gmail.com>
2026-01-21 16:30:54 +0530

    Add files via upload

---
+++ Conditions/grading_system.py
@@ -0,0 +1,14 @@
+marks = 76
+
+if marks >= 90:
+    grade = "A"
+elif marks >= 75:
+    grade = "B"
+elif marks >= 60:
+    grade = "C"
+elif marks >= 40:
+    grade = "D"
+else:
+    grade = "Fail"
+
+print("Grade:", grade)
