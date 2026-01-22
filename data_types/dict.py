eab1bc8e67881c6ac407284bb22226923030fb6d
Naina-tech-space <bnaina153@gmail.com>
2026-01-22 19:57:08 +0530

    used git commands

--- data_types/dict.py
+++ data_types/dict.py
@@ -1,13 +1,13 @@
 students = {
     "Naina": {"Math": 85, "Physics": 90, "Chemistry": 78},
-    "Riya": {"Math": 92, "Physics": 88, "Chemistry": 95},
-    "Aarav": {"Math": 79, "Physics": 85, "Chemistry": 80}
+    "ShivParvati": {"Math": 92, "Physics": 88, "Chemistry": 95},
+    "Nagaraj": {"Math": 79, "Physics": 85, "Chemistry": 80}
 }
 
 print("Naina's Physics marks:", students["Naina"]["Physics"])
 
 students["Kabir"] = {"Math": 88, "Physics": 90, "Chemistry": 85}
-students["Riya"]["Math"] = 95
+students["ShivParvati"]["Math"] = 95
 
 print("\nStudent Scores:")
 for student, scores in students.items():
@@ -15,7 +15,7 @@ for student, scores in students.items():
     for subject, score in scores.items():
         print(f"{subject}: {score}")
 
-if "Aarav" in students:
-    print("\nAarav's record found!")
+if "Nagaraj" in students:
+    print("\nNagaraj's record found!")
 
 print("\nAll students:", list(students.keys()))
